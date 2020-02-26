931d91c1ff769f17487e3569e2586d73f1259ab7
Зонов Олег <O.Zonov@DrWeb.com>
2020-02-26 22:16:52 +0300

    массивы

--- App.cs
+++ App.cs
@@ -1,14 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Threading;
+
 using static System.Math;
 
 class App
 {
-	byte[] Data;
-	String[] Strings;
+	readonly byte[] Data;
+	readonly int[] Starts;
+	readonly int[] Lengths;
 	readonly List<int>[] Track = new List<int>[] { new List<int>(), new List<int>() };
 	int Length;
 
@@ -17,150 +19,98 @@ class App
 	public App(byte[] data)
 	{
 		Data = data;
-		Strings = new String[data.Length];
-		Load();
+		Starts = new int[data.Length];
+		Lengths = new int[data.Length];
 	}
 
 	void Initialize()
 	{
-		var bytes = new int?[256];
-		var trackNew = Track[1 & 1];
-
-		Length = 1;
-		trackNew.Clear();
+		var bytes = new int[256];
+		var trackNew = Track[0 & 1];
 		for (var i = 0; i < Data.Length; i++)
 		{
-			if (bytes[Data[i]] is int start)
+			var start = bytes[Data[i]] - 1;
+			if (start < 0)
 			{
-				Debug.Assert(start < i);
-				Strings[i] = new String { App = this, Index = i, Start = start };
-				Strings[start].Count++;
-				trackNew.Add(i);
+				Starts[i] = i;
+				bytes[Data[i]] = i + 1;
 			}
 			else
 			{
-				Strings[i] = new String { App = this, Index = i, Start = i };
-				bytes[Data[i]] = i;
+				Starts[i] = start;
+				trackNew.Add(i);
+			}
+		}
+		Length = 1;
+	}
+
+	void Solve()
+	{
+		var remap = new ReMapDictionary();
+		for (Length = 1; Length < Data.Length; Length++)
+		{
+			var tracking = Track[Length & 1 ^ 1];
+			var trackNew = Track[Length & 1];
+			if (tracking.Count == 0) break;
+
+			remap.Clear();
+			trackNew.Clear();
+			for (var j = 0; j < tracking.Count; j++)
+			{
+				var i = tracking[j];
+				if (i + Length >= Data.Length)
+				{
+					Lengths[i] = Length;
+				}
+				else if (Data[i + Length] == Data[Starts[i] + Length])
+				{
+					trackNew.Add(i); // продолжаем отслѣживать
+				}
+				else if (remap.TryAdd(Starts[i], Data[i + Length], i, out var start))
+				{
+					Lengths[i] = Length; // предыдущий сегмент
+				}
+				else
+				{
+					Starts[i] = start; // перескок на другую вѣтвь
+					trackNew.Add(i);
+				}
 			}
 		}
 	}
 
 	void Run()
 	{
+		Load();
+
 		Initialize();
-		var abandoned = new HashSet<int>();
-		for (Length = 2; Length <= Data.Length - Length; Length++)
-		{
-			var tracking = Track[Length & 1 ^ 1];
-			var trackNew = Track[Length & 1];
-			if (tracking.Count == 0) break;
 
-			trackNew.Clear();
-			abandoned.Clear();
-			foreach (var i in tracking)
-			{
-				var prev = Strings[i].Start;
-				ref var current = ref Strings[i];
-				ref var initial = ref Strings[prev];
+		var thread = new Thread(ProgressProc) { IsBackground = true };
+		thread.Start();
 
-				if (i + Length - 1 >= Data.Length)
-				{
-					if (Length == 0x15483) Debugger.Break();
-					current.Length = Length - 1;
-					current.Another = prev;
-					Debug.Assert(initial.Count > 0);
-					initial.Count--;
-					if (initial.Count == 0)
-					{
-						initial.Length = Length - 1;
-						initial.Another = i;
-					}
-					continue;
-				}
+		Solve();
 
-				Debug.Assert(i > prev);
-				Debug.Assert(current.Count == 0);
-				Debug.Assert(current.Ends is null);
+		thread.Abort();
 
-				var ending = Data[i + Length - 1];
-				if (ending == Data[prev + Length - 1])
-				{
-					trackNew.Add(i);
-					continue;
-				}
-
-				current.Length = Length - 1;
-				current.Another = prev;
-				Debug.Assert(initial.Count > 0);
-				initial.Count--;
-				if (initial.Count == 0)
-				{
-					initial.Length = Length - 1;
-					initial.Another = i;
-				}
-
-				if (abandoned.Add(prev))
-				{
-					Debug.Assert(initial.Ends is null);
-					initial.Ends = new Dictionary<byte, int>();
-					initial.Ends.Add(ending, i);
-					current.Start = i;
-				}
-				else if (initial.Ends.TryGetValue(ending, out var start))
-				{
-					Strings[start].Count++;
-					current.Start = start;
-					trackNew.Add(i);
-					continue;
-				}
-				else
-				{
-					initial.Ends.Add(ending, i);
-					current.Start = i;
-				}
-			}
-
-			foreach (var item in abandoned)
-				Strings[item].Ends = null;
-
-			var added = tracking.Count - trackNew.Count;
-		//	Console.WriteLine($"{Length}: {trackNew.Count} - {added}");
-		}
 		Save();
-		var sorted = new String[Strings.Length]; Strings.CopyTo(sorted, 0); // 0x00015484
-		Array.Sort(sorted, (x, y) =>
-		{
-			int c;
-			if ((c = y.Length - x.Length) != 0) return c;
-			if ((c = x.Another - y.Another) != 0) return c;
-			return x.Index - y.Index;
-		});
-		var result = new List<String>();
-		for (var i = 0; i < sorted.Length; i++)
-			if (!result.Any(x => x.Start <= sorted[i].Index && sorted[i].Index <= x.Start + x.Length))
-				result.Add(sorted[i]);
+
+		var results = new List<String>();
+		var strings = Strings.ToList();
+		var set = new LCS.RangeSet();
+		foreach (var str in strings)
+			foreach (var start in str)
+				if (set.TryAddNotOverlapping(start, str.Length))
+					if (Data.Skip(start).Take(str.Length).Any(x => x == 0 || x == 0xFF))
+						results.Add(new String(start, str.Length));
 	}
 
 	void Load(string fileName = DefaultStateFileName)
 	{
 		try
 		{
-			using (var f = new BinaryReader(File.OpenRead(fileName)))
-			{
-				var strings = new String[f.BaseStream.Length / 16];
-				for (var i = 0; i < strings.Length; i++)
-				{
-					Strings[i] = new String
-					{
-						Start   = f.ReadInt32(),
-						Count   = f.ReadInt32(),
-						Length  = f.ReadInt32(),
-						Another = f.ReadInt32(),
-						Index = i,
-						App = this,
-					};
-				}
-			}
+			using var f = new BinaryReader(File.OpenRead(fileName));
+			for (var i = 0; i < Starts.Length; i++) Starts[i] = f.ReadInt32();
+			for (var i = 0; i < Lengths.Length; i++) Lengths[i] = f.ReadInt32();
 		}
 		catch (FileNotFoundException)
 		{
@@ -170,14 +120,33 @@ class App
 
 	void Save(string fileName = DefaultStateFileName)
 	{
-		using (var f = new BinaryWriter(File.OpenWrite(fileName)))
+		using var f = new BinaryWriter(File.OpenWrite(fileName));
+		for (var i = 0; i < Starts.Length; i++) f.Write(Starts[i]);
+		for (var i = 0; i < Lengths.Length; i++) f.Write(Lengths[i]);
+	}
+
+	IEnumerable<StringSet> Strings
+	{
+		get
 		{
-			for (var i = 0; i < Strings.Length; i++)
+			var indexes = new int[Data.Length];
+			for (var i = 0; i < indexes.Length; i++) indexes[i] = i;
+			Array.Sort(indexes, (x, y) =>
 			{
-				f.Write(Strings[i].Start);
-				f.Write(Strings[i].Count);
-				f.Write(Strings[i].Length);
-				f.Write(Strings[i].Another);
+				int c;
+				if ((c = Lengths[x] - Lengths[y]) != 0) return c;
+				if ((c =  Starts[y] -  Starts[x]) != 0) return c;
+				return y - x;
+			});
+
+			for (int i = indexes.Length - 1, j = i - 1; i >= 0; i = j)
+			{
+				var s = new StringSet(Lengths[indexes[i]], Starts[indexes[i]]) { Starts[indexes[i]], indexes[i] };
+				for (; j >= 0; j--)
+					if (Lengths[indexes[j]] == s.Length && Starts[indexes[j]] == s.Source)
+						s.Add(indexes[j]);
+					else break;
+				yield return s;
 			}
 		}
 	}
@@ -185,17 +154,68 @@ class App
 	struct String
 	{
 		public int Start;
-		public int Count;
 		public int Length;
-		public int Another;
-		public int Index;
-		public App App;
-		public Dictionary<byte, int> Ends;
 
-		public override string ToString() => $"{Index:x}: {Start:x} × {Count} \"" + string.Join(" ", App.Data.Skip(Start).Take(Min(8, Min(App.Length, App.Data.Length - Start))).Select(x => x.ToString("X2"))) + "\"";
+		public String(int start, int length)
+		{
+			Start = start;
+			Length = length;
+		}
 	}
 
-	public override string ToString() => $"{Length}, Track={Max(Track[0].Count, Track[1].Count)} d={Abs(Track[0].Count - Track[1].Count)}";
+	class StringSet : List<int>
+	{
+		public int Length;
+		public int Source;
+
+		public StringSet(int length, int source)
+		{
+			Length = length;
+			Source = source;
+		}
+	}
+
+	class ReMapDictionary : Dictionary<ReMapKey, int>
+	{
+		public bool TryAdd(int source, byte ending, int index, out int start)
+		{
+			var key = new ReMapKey(source, ending);
+			if (TryGetValue(key, out start)) return false;
+			else Add(key, start = index);
+			return true;
+		}
+	}
+
+	struct ReMapKey : IEquatable<ReMapKey>
+	{
+		public int  Source;
+		public byte Ending;
+
+		public ReMapKey(int source, byte ending)
+		{
+			Source = source;
+			Ending = ending;
+		}
+
+		public bool Equals(ReMapKey other) => Source == other.Source && Ending == other.Ending;
+		public override bool Equals(object obj) => obj is ReMapKey ending && Equals(ending);
+		public override int GetHashCode() => Source << 8 | Ending;
+		public override string ToString() => $"{Source:x} [{Ending:X2}]";
+	}
+
+	void ProgressProc()
+	{
+		var t0 = DateTime.Now;
+		for (var n1 = Max(Track[0].Count, Track[1].Count); n1 != 0;)
+		{
+			var n2 = Max(Track[0].Count, Track[1].Count);
+			Console.WriteLine($"{Length}: {n2} - {n1 - n2}" + " за " + (DateTime.Now - t0).TotalSeconds);
+			Thread.Sleep(200);
+			n1 = n2;
+		}
+	}
+
+	public override string ToString() => $"{Length}, Track={Max(Track[0].Count, Track[1].Count)}";
 
 	static void Main(string[] args) => new App(File.ReadAllBytes(args[0])).Run();
 }
